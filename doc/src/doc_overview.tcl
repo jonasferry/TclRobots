d64bb7722f87dfcc6032f099bcbb22d8f4a358b6
Jonas Ferry <jonas.ferry@gmail.com>
2010-03-09 22:21:51 +0100

    Generated new documentation.

---
+++ doc/src/doc_overview.tcl
@@ -0,0 +1,15 @@
+proc doc_overview {docs} {
+    return "
+<div id='logo'>
+<a name='robo_top_of_doc'></a>
+
+</div> <!-- logo -->
+<h3>DOCUMENTATION OVERVIEW</h3>
+<pre>
+$docs
+</pre>
+<div id='footer'>
+<p>&nbsp;</p>
+</div> <!-- footer -->
+"
+}
