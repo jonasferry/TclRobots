11f4f18a71698a99e0e79e44698fc69c4ce6d2a8
Jonas Ferry <jonas.ferry@gmail.com>
2010-03-13 21:00:38 +0100

    Reworked documentation to use single HTML source

--- syscalls.tcl
+++ syscalls.tcl
@@ -76,19 +76,7 @@ proc _syscall_yield {args} {
 }
 #******
 
-#****I* syscalls/basic_cmds
-#
-# NAME
-#
-#   basic_cmds
-#
-# DESCRIPTION
-#
-#   Basic commands
-#
-#******
-
-#****iI* syscalls/basic
+#****I* syscalls/basic
 #
 # NAME
 #
@@ -100,26 +88,7 @@ proc _syscall_yield {args} {
 #
 #******
 
-#****iS* basic/scanner
-#
-# NAME
-#
-#   scanner degree resolution
-#
-# DESCRIPTION
-#
-#   The scanner command invokes the robot's scanner. Degree must be in
-#   the range 0-359. Scanner returns 0 if nothing found, or an integer
-#   greater than zero indicating the distance to an opponent. Resolution
-#   controls how wide in degrees the scan can detect opponents from the
-#   absolute scanning direction, and must be in the range 0-10. A robot
-#   that has been destroyed is not reported by the scanner.
-#
-#   Cost: 2 ticks.
-#
-#******
-
-#****P* basic_cmds/scanner
+#****P* basic/scanner
 #
 # NAME
 #
@@ -137,26 +106,7 @@ proc scanner {degree resolution} {
 }
 #******
 
-#****iS* basic/dsp
-#
-# NAME
-#
-#   dsp
-#
-# DESCRIPTION
-#
-#   The dsp command returns a list of two integers, the first element is
-#   the digital signature of the last robot found using the scanner.
-#   The second element is the percent of damage the scanned robot has
-#   accumulated, 0-99 percent. Each robot in a battle has a distinct
-#   signature. If nothing was found during the last scan (scanner
-#   command returned 0), then the dsp command will return "0 0".
-#
-#   Cost: 1 tick.
-#
-#******
-
-#****P* basic_cmds/dsp
+#****P* basic/dsp
 #
 # NAME
 #
@@ -173,25 +123,7 @@ proc dsp {} {
 }
 #******
 
-#****iS* basic/alert
-#
-# NAME
-#
-#   alert proc-name
-#
-# DESCRIPTION
-#
-#   The alert command names a procedure to be called when the robot is
-#   being scanned by another robot. When the robot detects it has been
-#   scanned, the proc-name procedure is called with one argument, the
-#   dsp signature of the robot that performed the scan. If proc-name is
-#   null (""), then the alert feature is disabled.
-#
-#   Cost: 1 tick.
-#
-#******
-
-#****P* basic_cmds/alert
+#****P* basic/alert
 #
 # NAME
 #
@@ -208,23 +140,7 @@ proc alert {procname} {
 }
 #******
 
-#****iS* basic/cannon
-#
-# NAME
-#
-#   cannon degree range
-#
-# DESCRIPTION
-#
-#   The cannon commands fires a shell in the direction specified by
-#   degree, for the distance range. Cannon returns 1 if a shell was
-#   fired; if the cannon is reloading, 0 is returned.
-#
-#   Cost: 1 tick.
-#
-#******
-
-#****P* basic_cmds/cannon
+#****P* basic/cannon
 #
 # NAME
 #
@@ -241,19 +157,7 @@ proc cannon {degree range} {
 }
 #******
 
-#****iS* basic/health
-#
-# NAME
-#
-#   health
-#
-# DESCRIPTION
-#
-#   Cost: 1 tick.
-#
-#******
-
-#****P* basic_cmds/health
+#****P* basic/health
 #
 # NAME
 #
@@ -270,28 +174,7 @@ proc health {} {
 }
 #******
 
-#****iS* basic/drive
-#
-# NAME
-#
-#   drive degree speed
-#
-# DESCRIPTION
-#
-#   The drive command starts the robot's drive mechanism. Degree must
-#   be in the range 0-359. Speed must be in the range 0-100. Any
-#   change in course that falls outside the "Degrees of course change"
-#   table (see "The Robot", above) will cause the robot's speed to be
-#   set to 0 along the current course. A speed of 0 causes the robot to
-#   coast to a stop. The drive command returns the speed set. If the
-#   drive is currently overheated, the maximum speed during overheating
-#   (35%) will be set.
-#
-#   Cost: 1 tick.
-#
-#******
-
-#****P* basic_cmds/drive
+#****P* basic/drive
 #
 # NAME
 #
@@ -308,24 +191,7 @@ proc drive {degree speed} {
 }
 #******
 
-#****iS* basic/speed
-#
-# NAME
-#
-#   speed
-#
-# DESCRIPTION
-#
-#   The speed command reports the current speed of the robot, 0-100.
-#   Speed may return more or less than what was last set with the drive
-#   command because of acceleration/deaccelearation, drive overheating,
-#   or collision into a wall.
-#
-#   Cost: 1 tick.
-#
-#******
-
-#****P* basic_cmds/speed
+#****P* basic/speed
 #
 # NAME
 #
@@ -342,24 +208,7 @@ proc speed {} {
 }
 #******
 
-#****iS* basic/heat
-#
-# NAME
-#
-#   heat
-#
-# DESCRIPTION
-#
-#   The heat command returns a list of two integers, the first element
-#   is the overheating flag, 1 if the maximum motor heat value was
-#   attained, otherwise 0. The second element is the current motor heat
-#   index, 0-200.
-#
-#   Cost: 1 tick.
-#
-#******
-
-#****P* basic_cmds/heat
+#****P* basic/heat
 #
 # NAME
 #
@@ -376,22 +225,7 @@ proc heat {} {
 }
 #******
 
-#****iS* basic/loc_x
-#
-# NAME
-#
-#   loc_x
-#
-# DESCRIPTION
-#
-#   The loc_x command returns the current x axis location of the robot,
-#   0-999 meters.
-#
-#   Cost: 1 tick.
-#
-#******
-
-#****P* basic_cmds/loc_x
+#****P* basic/loc_x
 #
 # NAME
 #
@@ -408,22 +242,7 @@ proc loc_x {} {
 }
 #******
 
-#****iS* basic/loc_y
-#
-# NAME
-#
-#   loc_y
-#
-# DESCRIPTION
-#
-#   The loc_y command returns the current y axis location of the robot,
-#   0-999 meters.
-#
-#   Cost: 1 tick.
-#
-#******
-
-#****P* basic_cmds/loc_y
+#****P* basic/loc_y
 #
 # NAME
 #
@@ -440,22 +259,7 @@ proc loc_y {} {
 }
 #******
 
-#****iS* basic/tick
-#
-# NAME
-#
-#   tick
-#
-# DESCRIPTION
-#
-#   The tick command returns the current clock tick. The clock tick is
-#   set to 0 at game startup.
-#
-#   Cost: 1 tick.
-#
-#******
-
-#****P* basic_cmds/tick
+#****P* basic/tick
 #
 # NAME
 #
@@ -472,19 +276,7 @@ proc tick {} {
 }
 #******
 
-#****I* syscalls/team_cmds
-#
-# NAME
-#
-#   team_cmds
-#
-# DESCRIPTION
-#
-#   Team commands
-#
-#******
-
-#****iI* syscalls/team
+#****I* syscalls/team
 #
 # NAME
 #
@@ -496,24 +288,7 @@ proc tick {} {
 #
 #******
 
-#****iS* team/team_declare
-#
-# NAME
-#
-#   team_declare teamname
-#
-# DESCRIPTION
-#
-#   The team_declare command sets the team alliance to the teamname
-#   argument. team_declare is only effective the first time it is
-#   executed in a robot control program. team_declare returns the
-#   teamname value.
-#
-#   Cost: 1 tick.
-#
-#******
-
-#****P* team_cmds/team_declare
+#****P* team/team_declare
 #
 # NAME
 #
@@ -530,24 +305,7 @@ proc team_declare {teamname} {
 }
 #******
 
-#****iS* team/team_send
-#
-# NAME
-#
-#   team_send data
-#
-# DESCRIPTION
-#
-#   The team_send command makes the argument available to all other
-#   robots with the same teamname. Data is a single string argument, and
-#   can be any value or list. If a team has not been declared with the
-#   team_declare command, the team_send command has no effect.
-#
-#   Cost: 0 ticks.
-#
-#******
-
-#****P* team_cmds/team_send
+#****P* team/team_send
 #
 # NAME
 #
@@ -564,28 +322,7 @@ proc team_send {data} {
 }
 #******
 
-#****iS* team/team_get
-#
-# NAME
-#
-#   team_get
-#
-# DESCRIPTION
-#
-#   The team_get command returns a list of team members and their
-#   current data values. Each element of the list returned is a list of
-#   the digital signature of a team member and that robot's last
-#   team_send data value. If a team has not been declared with
-#   team_declare or all of the other members of the team have not
-#   declared a team or are dead, the return value is an null list. The
-#   robot executing the team_get command is also excluded from the
-#   return list.
-#
-#   Cost: 0 ticks.
-#
-#******
-
-#****P* team_cmds/team_get
+#****P* team/team_get
 #
 # NAME
 #
@@ -602,19 +339,7 @@ proc team_get {} {
 }
 #******
 
-#****I* syscalls/convenience_cmds
-#
-# NAME
-#
-#   convenience_cmds
-#
-# DESCRIPTION
-#
-#   Convenience commands
-#
-#******
-
-#****iI* syscalls/convenience
+#****I* syscalls/convenience
 #
 # NAME
 #
@@ -626,22 +351,7 @@ proc team_get {} {
 #
 #******
 
-#****iS* convenience/dputs
-#
-# NAME
-#
-#   dputs args
-#
-# DESCRIPTION
-#
-#   The dputs command prints a message in the GUI message window.
-#   Dputs accepts any number of arguments.
-#
-#   Cost: 0 ticks.
-#
-#******
-
-#****P* convenience_cmds/dputs
+#****P* convenience/dputs
 #
 # NAME
 #
@@ -658,25 +368,7 @@ proc dputs {args} {
 }
 #******
 
-#****iS* convenience/rand
-#
-# NAME
-#
-#   rand max
-#
-# DESCRIPTION
-#
-#   The rand command is a simple random number generator, based on
-#   Knuth's algorithm. The rand command returns an integer between 0 and
-#   ( max - 1), where max is in the range 1 to 65535. The seed value is
-#   randomly set; if a particular seed value is desired, the global
-#   variable _lastvalue should be set to some starting value.
-#
-#   Cost: 0 ticks.
-#
-#******
-
-#****P* convenience_cmds/rand
+#****P* convenience/rand
 #
 # NAME
 #
@@ -693,19 +385,7 @@ proc rand {max} {
 }
 #******
 
-#****iS* convenience/callback
-#
-# NAME
-#
-#   callback time script
-#
-# DESCRIPTION
-#
-#   Cost: 0 ticks.
-#
-#******
-
-#****P* convenience_cmds/callback
+#****P* convenience/callback
 #
 # NAME
 #
@@ -722,11 +402,11 @@ proc callback {time script} {
 }
 #******
 
-#****I* syscalls/deprecated_cmds
+#****I* syscalls/deprecated
 #
 # NAME
 #
-#   deprecated_cmds
+#   deprecated
 #
 # DESCRIPTION
 #
@@ -734,7 +414,7 @@ proc callback {time script} {
 #
 #******
 
-#****P* deprecated_cmds/after
+#****P* deprecated/after
 #
 # NAME
 #
@@ -763,7 +443,7 @@ proc after {ms args} {
 }
 #******
 
-#****P* deprecated_cmds/damage
+#****P* deprecated/damage
 #
 # NAME
 #
