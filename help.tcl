b40539f8a3364cff733730b3ff86175398fc72b8
Jonas Ferry <jonas.ferry@gmail.com>
2010-03-16 20:20:14 +0100

    Added navigation to Help browser and added handling of license link

--- help.tcl
+++ help.tcl
@@ -37,23 +37,39 @@ proc init_help {} {
     toplevel .help
     grid columnconfigure .help 0 -weight 1; grid rowconfigure .help 0 -weight 1
 
-    set html_help 0
+    # Create navigation
+    set ::helpnav_f [ttk::frame .help.f1]
+    set ::help_b    [ttk::button $::helpnav_f.b1 -text "Show Help" \
+                         -command {show_page help}]
+    set ::license_b [ttk::button $::helpnav_f.b2 -text "Show License" \
+                         -command {show_page license}]
+    set ::close_b   [ttk::button $::helpnav_f.b3 -text "Close" \
+                         -command {end_help}]
 
-    # Load the extension
+    # Grid the buttons
+    grid $::help_b    -column 0 -row 0 -sticky w
+    grid $::license_b -column 1 -row 0 -sticky w
+    grid $::close_b   -column 2 -row 0 -sticky w
+
+    # Load the HTML extension
+    set ::html_help 0
+
     switch $::tcl_platform(platform) {
         windows {
-            load $::thisDir/include/tkhtml/tkhtml.dll
-            set html_help 1
+            if {![catch {load $::thisDir/include/tkhtml/tkhtml.dll}] } {
+                set ::html_help 1
+            }
         }
         unix {
-            load $::thisDir/include/tkhtml/tkhtml.so
-            set html_help 1
+            if {![catch {load $::thisDir/include/tkhtml/tkhtml.so}] } {
+                set ::html_help 1
+            }
         }
     }
-    if {$html_help} {
+    if {$::html_help} {
         # HTML is enabled, create HTML widget
         set ::help_t [html .help.t -hyperlinkcommand handle_link \
-                          -base $::thisDir/doc/help_doc.html \
+                          -base $::thisDir/doc/game_doc.html \
                           -yscrollcommand ".help.s set"]
 
         # Bind mouse clicks
@@ -67,31 +83,92 @@ proc init_help {} {
         bind all <MouseWheel> "+handle_scrollwheel %X %Y %D"
 
         # Read the HTML help doc
-        set f    [open $::thisDir/doc/help_doc.html]
-        set text [read $f]
+        set f [open $::thisDir/doc/game_doc.html]
+        set ::help_text [read $f]
         close $f
 
-        # Insert text into HTML widget
-        $::help_t parse $text
+        # Read the license doc, add <pre> formatting
+        set f [open $::thisDir/LICENSE]
+        set ::license_text "<pre>[read $f]</pre>"
+        close $f
     } else {
         # HTML is disabled, create text widget
-        set help_t [tk::text .help.t -width 80 -height 40 -wrap word \
+        set ::help_t [tk::text .help.t -width 80 -height 30 -wrap word \
                         -yscrollcommand ".help.s set"]
 
-        # Read the ASCII help doc
-        set f    [open $::thisDir/README]
-        set text [read $f]
+        # Read the plain text help doc
+        set f [open $::thisDir/README]
+        set ::help_text [read $f]
         close $f
 
-        # Insert text into text box
-        $help_t insert 1.0 $text
+        # Read the license doc
+        set f [open $::thisDir/LICENSE]
+        set ::license_text [read $f]
+        close $f
     }
+
+    # Create scrollbar for help doc window
     set help_s [ttk::scrollbar .help.s -command ".help.t yview" \
                     -orient vertical]
 
     # Grid the text box and scrollbar
-    grid $::help_t -column 0 -row 0 -sticky nsew
-    grid $help_s -column 1 -row 0 -sticky ns
+    grid $::helpnav_f -column 0 -row 0 -sticky nsew
+    grid $::help_t    -column 0 -row 1 -sticky nsew
+    grid $help_s      -column 1 -row 1 -sticky ns
+
+    show_page help
+}
+#******
+
+#****P* init_help/show_page
+#
+# NAME
+#
+#   show_page
+#
+# SYNOPSIS
+#
+#   show_page page
+#
+# DESCRIPTION
+#
+#   Show the requested page text in Help browser. Checks if HTML is
+#   enabled or not.
+#
+# SOURCE
+#
+proc show_page {page} {
+    if {$page eq "help"} {
+        set text $::help_text
+    } else {
+        set text $::license_text
+    }
+    if {$::html_help} {
+        # Clear and insert new text into HTML widget
+        $::help_t clear
+        $::help_t parse $text
+    } else {
+        # Insert text into text box
+        $::help_t delete 1.0 end
+        $::help_t insert 1.0 $text
+    }
+}
+#******
+
+#****P* init_help/end_help
+#
+# NAME
+#
+#   end_help
+#
+# DESCRIPTION
+#
+#   Closes the help browser.
+#
+# SOURCE
+#
+proc end_help {} {
+    destroy .help
 }
 #******
 
@@ -111,13 +188,22 @@ proc init_help {} {
 #   y coordinates of the click. Uses href widget command to figure out
 #   if a link was clicked.
 #
-#   Currently only handles anchor links.
+#   Currently handles anchor links and the license link.
 #
 # SOURCE
 #
 proc handle_click {x y} {
-    set link [$::help_t href $x $y]
-    $::help_t yview [lindex [split $link #] 1]
+    # Must make sure that Close button has not destroyed the widget
+    # before accessing it
+    if {[winfo exists $::help_t]} {
+        set link [$::help_t href $x $y]
+        if {$link eq \
+                "http://github.com/jonasferry/TclRobots/blob/master/LICENSE"} {
+            show_page license
+        } else {
+            $::help_t yview [lindex [split $link #] 1]
+        }
+    }
 }
 #******
 
