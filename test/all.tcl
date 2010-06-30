b7be72eaeef273fe9f6f1b40d7dd6eaefdaf0008
Jonas Ferry <jonas.ferry@gmail.com>
2010-06-30 21:34:05 +0200

    Started test suite

---
+++ test/all.tcl
@@ -0,0 +1,9 @@
+package require tcltest
+namespace import ::tcltest::*
+
+set ::testDir [file dirname [file normalize [info script]]]
+puts "testDir $::testDir"
+
+configure -testdir $::testDir
+eval configure $argv
+runAllTests
